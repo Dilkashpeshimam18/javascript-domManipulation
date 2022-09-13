701a07f2ad70fc0f0dd51dcea44d9a23b2514fe0
Dilkash <dilkashpeshimam@gmail.com>
2022-09-13 13:30:23 +0530

    second commit

---
+++ index3.js
@@ -0,0 +1,14 @@
+let button=document.querySelector('.btn')
+let username=document.querySelector('#user')
+let email=document.querySelector('#email')
+
+button.addEventListener('click',(e)=>{
+    e.preventDefault()
+    if(username.value==='' || email.value===''){
+        alert('Please enter the value')
+    }else{
+        document.querySelector('.user-name').innerHTML=username.value
+        document.querySelector('.user-email').innerHTML=email.value
+    }
+    
+})
