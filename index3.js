5870d348825a14787a29cde512b8c86a4360a6a4
Dilkash <dilkashpeshimam@gmail.com>
2022-09-15 18:24:41 +0530

    displaying users from local storage

--- index3.js
+++ index3.js
@@ -1,23 +1,55 @@
 let button=document.querySelector('.btn')
 let username=document.querySelector('#user')
 let email=document.querySelector('#email')
-let user
-
+let users=[]
+let userObj={}
 button.addEventListener('click',(e)=>{
     e.preventDefault()
     if(username.value==='' || email.value===''){
         alert('Please enter the value')
     }else{
-        user={
+        userObj={
             name:username.value,
             email:email.value
         }
-        document.querySelector('.user-name').innerHTML=username.value
-        document.querySelector('.user-email').innerHTML=email.value
-        localStorage.setItem('user', JSON.stringify(user))
-        // localStorage.setItem('email', userEmail)
+        localStorage.setItem(`user${userObj.name}`, JSON.stringify(userObj))
+        displayUser(userObj)      
 
     }
-    console.log(JSON.parse(localStorage.getItem('user',user)))
 })
+
+document.addEventListener('DOMContentLoaded',()=>{
+
+    Object.keys(localStorage).forEach(function(key){
+        console.log(JSON.parse(localStorage.getItem(key)))
+       let userDetail=JSON.parse(localStorage.getItem(key))
+       users.push(userDetail)
+      
+      
+        
+        
+     });
+ 
+     users.forEach((user)=>{
+        let usersList=document.querySelector('.user-detail')
+
+     let userLi=document.createElement('li')
+     userLi.appendChild(document.createTextNode(`${user.name} ${user.email}`))
+     usersList.appendChild(userLi)
+})
+
+ })
+function displayUser(useObj){
+  
+  
+   
+     
+         let userList=document.querySelector('.user-detail')
+         let userTag=`<li> ${userObj.name} ${userObj.email} </li>`
+         userList.innerHTML= userList.innerHTML + userTag
+     
+ 
+}
+
+
 
