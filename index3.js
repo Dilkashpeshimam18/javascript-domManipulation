554a420852fc297f65a74b1808ececadbc78c0e3
Dilkash <dilkashpeshimam@gmail.com>
2022-09-15 14:29:36 +0530

    storing object in local storage

--- index3.js
+++ index3.js
@@ -1,18 +1,23 @@
 let button=document.querySelector('.btn')
 let username=document.querySelector('#user')
 let email=document.querySelector('#email')
+let user
 
 button.addEventListener('click',(e)=>{
     e.preventDefault()
     if(username.value==='' || email.value===''){
         alert('Please enter the value')
     }else{
+        user={
+            name:username.value,
+            email:email.value
+        }
         document.querySelector('.user-name').innerHTML=username.value
         document.querySelector('.user-email').innerHTML=email.value
-        localStorage.setItem('username', username.value)
-        localStorage.setItem('email', email.value)
+        localStorage.setItem('user', JSON.stringify(user))
+        // localStorage.setItem('email', userEmail)
 
     }
-    
+    console.log(JSON.parse(localStorage.getItem('user',user)))
 })
 
