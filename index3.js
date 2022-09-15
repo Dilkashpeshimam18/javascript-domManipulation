8929e32924cd673a4200ceec9270651420fe7eb6
Dilkash <dilkashpeshimam@gmail.com>
2022-09-15 20:50:23 +0530

    added delete fumctionality

--- index3.js
+++ index3.js
@@ -34,22 +34,51 @@ document.addEventListener('DOMContentLoaded',()=>{
         let usersList=document.querySelector('.user-detail')
 
      let userLi=document.createElement('li')
+     userLi.id=`user${user.name}`
      userLi.appendChild(document.createTextNode(`${user.name} ${user.email}`))
+     let deleteBtn=document.createElement('button')
+     deleteBtn.onclick=function(){
+        localStorage.removeItem(`user${user.name}`)
+        let ulList=document.querySelector('.user-detail')
+        let liToDelete=document.getElementById(`user${user.name}`)
+        console.log(liToDelete)
+        ulList.removeChild(liToDelete)
+     }
+
+   deleteBtn.appendChild(document.createTextNode('Delete'))
+   let editBtn=document.createElement('button')
+   editBtn.appendChild(document.createTextNode('Edit'))
+   userLi.appendChild(deleteBtn)
+   userLi.appendChild(editBtn)
+
      usersList.appendChild(userLi)
 })
 
  })
 function displayUser(useObj){
   
-  
+     if(localStorage.getItem(`user${userObj.name}`) !==null){
+     }
    
-     
+
          let userList=document.querySelector('.user-detail')
-         let userTag=`<li> ${userObj.name} ${userObj.email} </li>`
-         userList.innerHTML= userList.innerHTML + userTag
+         let userTag=`<li id='user${userObj.name}'> ${userObj.name} ${userObj.email} <button onClick=deleteUser('user${userObj.name}')>Delete User</button></li> `
+         userList.innerHTML= userList.innerHTML + userTag 
+        
+        
      
  
 }
 
 
+
+
+
+function deleteUser(userName){
+    console.log(userName)
+   localStorage.removeItem(userName)
+   let ulList=document.querySelector('.user-detail')
+   let liToDelete=document.getElementById(userName)
+   ulList.removeChild(liToDelete)
+}
 
